c33ec7ed1c656d76cd4a9bb34f2b8003926cad31
Neelam Saikrishna <nskrishna644@gmail.com>
2025-10-20 08:27:01 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -98,7 +98,7 @@ function NavMenu() {
                   </NavigationMenuContent>
                 </>
               ) : (
-                <Link href={link.href} legacyBehavior passHref>
+                <Link href={link.href}>
                   <NavigationMenuLink
                     active={pathname === link.href}
                     className={cn(
