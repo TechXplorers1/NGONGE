103b5893a72331653fbdbcca90430d86b20943ae
Pedda Narasappa Gari Naresh <devnareshtx@gmail.com>
2025-10-20 07:59:12 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,4 @@
 
-
 "use client";
 
 import Link from "next/link";
@@ -24,14 +23,13 @@ import {
 } from "@/components/ui/navigation-menu";
 
 const ListItem = React.forwardRef<
-  HTMLAnchorElement,
-  React.ComponentPropsWithoutRef<"a"> & { title: string, href: string }
->(({ className, title, children, href, ...props }, ref) => {
+  React.ElementRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link> & { title: string }
+>(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
         <Link
-          href={href}
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -99,7 +97,7 @@ function NavMenu() {
                   </NavigationMenuContent>
                 </>
               ) : (
-                <Link href={link.href} passHref legacyBehavior>
+                <Link href={link.href} legacyBehavior passHref>
                   <NavigationMenuLink
                     active={pathname === link.href}
                     className={cn(
