8dbce7bb691e4199a0c76168696a45aace0b7dcb
Neelam Saikrishna <nskrishna644@gmail.com>
2025-10-20 08:33:54 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -97,20 +97,19 @@ function NavMenu() {
                   </NavigationMenuContent>
                 </>
               ) : (
-                <NavigationMenuLink asChild active={pathname === link.href}>
-                  <Link
-                    href={link.href}
-                    className={cn(
-                      navigationMenuTriggerStyle(),
-                      "transition-colors hover:text-accent font-medium tracking-widest uppercase bg-transparent text-sm",
-                      pathname === link.href
-                        ? "text-primary"
-                        : "text-muted-foreground"
-                    )}
-                  >
-                    {link.name}
-                  </Link>
-                </NavigationMenuLink>
+                <Link href={link.href} passHref legacyBehavior>
+                  <NavigationMenuLink asChild active={pathname === link.href}>
+                    <a className={cn(
+                        navigationMenuTriggerStyle(),
+                        "transition-colors hover:text-accent font-medium tracking-widest uppercase bg-transparent text-sm",
+                        pathname === link.href
+                          ? "text-primary"
+                          : "text-muted-foreground"
+                      )}>
+                      {link.name}
+                    </a>
+                  </NavigationMenuLink>
+                </Link>
               )}
           </NavigationMenuItem>
         ))}
