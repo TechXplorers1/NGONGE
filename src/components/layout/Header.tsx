0e8ea1c5109cdda8881ca04e91d4266bec21da80
Pedda Narasappa Gari Naresh <devnareshtx@gmail.com>
2025-10-20 07:23:36 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,23 +28,22 @@ const ListItem = React.forwardRef<
 >(({ className, title, children, href, ...props }, ref) => {
   return (
     <li>
-      <Link href={href!} passHref legacyBehavior>
-        <NavigationMenuLink asChild>
-            <a
-            ref={ref}
-            className={cn(
-                "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                className
-            )}
-            {...props}
-            >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                {children}
-            </p>
-            </a>
-        </NavigationMenuLink>
-      </Link>
+      <NavigationMenuLink asChild>
+        <Link
+          href={href!}
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
     </li>
   );
 });
@@ -79,17 +78,18 @@ function NavMenu() {
                   </NavigationMenuContent>
                 </>
               ) : (
-                <Link href={link.href} passHref legacyBehavior>
-                  <NavigationMenuLink asChild active={pathname === link.href} className={cn(
+                <Link href={link.href} legacyBehavior={false}>
+                  <NavigationMenuLink
+                    active={pathname === link.href}
+                    className={cn(
                       navigationMenuTriggerStyle(),
                       "transition-colors hover:text-accent font-medium tracking-widest uppercase bg-transparent text-sm",
                       pathname === link.href
                         ? "text-primary"
                         : "text-muted-foreground"
-                    )}>
-                      <a>
-                        {link.name}
-                      </a>
+                    )}
+                  >
+                      {link.name}
                   </NavigationMenuLink>
                 </Link>
               )}
